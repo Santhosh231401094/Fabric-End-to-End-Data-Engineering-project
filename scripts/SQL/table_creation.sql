be98715b13d8676bccfabfa93a5d37fcdd80a599
Santhosh S <231401094@rajalakshmi.edu.in>
2025-12-29 17:09:16 +0530

    Create table_creation.sql

---
+++ scripts/SQL/table_creation.sql
@@ -0,0 +1,14 @@
+--DROP TABLE source_cars
+CREATE TABLE source_cars(
+Branch_ID VARCHAR(200),
+Dealer_ID VARCHAR(200),
+Model_ID VARCHAR(200),
+Revenue BIGINT,
+Units_sold BIGINT,
+Date_ID VARCHAR(200),
+Day INT,
+Month INT,
+Year INT,
+BranchName VARCHAR(2000),
+DealerName VARCHAR(2000)
+)
